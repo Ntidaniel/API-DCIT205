b48b763a2b4785076fe1522348b971ab02f9b43c
11137328 Daniel <danielgodfred12@gmail.com>
2023-12-18 08:46:39 +0000

    my updated index.js

--- index.js
+++ index.js
@@ -15,3 +15,46 @@ app.use(bodyParser.json());
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
+const patientSchema = new mongoose.Schema({
+    patientId: String,
+    surname: String,
+    othernames: String,
+    gender: String,
+    phoneNumber: String,
+    residentialAddress: String,
+    emergency: {
+      name: String,
+      contact: String,
+      relationship: String
+    }
+  });
+  
+  const Patient = mongoose.model('Patient', patientSchema);
+
+// Register a new patient
+app.post('/registerPatient', async (req, res) => {
+    try {
+      const { patientId, surname, othernames, gender, phoneNumber, residentialAddress, emergency } = req.body;
+      const patient = new Patient({
+        patientId,
+        surname,
+        othernames,
+        gender,
+        phoneNumber,
+        residentialAddress,
+        emergency
+      });
+  
+      await patient.save();
+      res.status(201).json({ message: 'Patient registered successfully' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+  
+  // Add more routes for other functionalities...
+  
+
